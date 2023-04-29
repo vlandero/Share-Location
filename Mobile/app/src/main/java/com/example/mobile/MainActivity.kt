37e136f74cb138d73904de51b90b244606a2dc10
vlandero <vlad.andrei.rosu@gmail.com>
2023-04-29 15:19:38 +0300

    first commit with test api

--- Mobile/app/src/main/java/com/example/mobile/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/mobile/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mobile
 
+import ApiCall
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobile.DTOs.UserRegisterRequestDTO
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val apiButton = findViewById<android.widget.Button>(R.id.buttonApi);
+        apiButton.setOnClickListener {
+            println("Button clicked")
+            val testDto = UserRegisterRequestDTO("test", "test", "test", "test", "test", arrayListOf<String>("test","test"), "test", "test")
+            val thread = ApiCall().registerUser(testDto)
+            thread.start()
+        }
     }
 }
