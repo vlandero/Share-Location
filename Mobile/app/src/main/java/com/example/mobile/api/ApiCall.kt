30d6ff166679e53c712110f6e1615be31b5cb1ec
vlandero <vlad.andrei.rosu@gmail.com>
2023-05-02 16:45:41 +0300

    new api calls

--- Mobile/app/src/main/java/com/example/mobile/api/ApiCall.kt
+++ Mobile/app/src/main/java/com/example/mobile/api/ApiCall.kt
@@ -1,5 +1,7 @@
+import com.example.mobile.DTOs.ManyToManyDTO
 import com.example.mobile.DTOs.UserLoginRequestDTO
 import com.example.mobile.DTOs.UserRegisterRequestDTO
+import com.example.mobile.DTOs.UserToBeStoredDTO
 import com.example.mobile.secrets.Secrets
 import com.google.gson.Gson
 import kotlinx.coroutines.*
@@ -13,7 +15,7 @@ class ApiCall {
     private val mainUrl: String = secrets.API_URL + "/api/"
     private val client = OkHttpClient()
 
-    private fun apiCall(json: String, url: String, callback: (String?, Exception?) -> Unit) {
+    private fun postApiCall(json: String, url: String, callback: (String?, Exception?) -> Unit) {
         try {
             val requestBody =
                 json.toRequestBody("application/json; charset=utf-8".toMediaType())
@@ -33,13 +35,51 @@ class ApiCall {
         }
     }
 
+    private fun getApiCall(url: String, callback: (String?, Exception?) -> Unit) {
+        try {
+            val request = Request.Builder()
+                .url(url)
+                .get()
+                .build()
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                callback(response.body.string(), null)
+            } else {
+                val responseBody = response.body.string()
+                callback(null, Exception(responseBody ?: "Unknown Error"))
+            }
+        } catch (e: Exception) {
+            callback(null, e)
+        }
+    }
+
+    private fun deleteApiCall(json: String, url: String, callback: (String?, Exception?) -> Unit) {
+        try {
+            val requestBody =
+                json.toRequestBody("application/json; charset=utf-8".toMediaType())
+            val request = Request.Builder()
+                .url(url)
+                .delete(requestBody)
+                .build()
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                callback(response.body.string(), null)
+            } else {
+                val responseBody = response.body.string()
+                callback(null, Exception(responseBody ?: "Unknown Error"))
+            }
+        } catch (e: Exception) {
+            callback(null, e)
+        }
+    }
+
     @OptIn(DelicateCoroutinesApi::class)
     fun registerUserAsync(dto: UserRegisterRequestDTO, callback: (String?, Exception?) -> Unit) {
         val url = mainUrl + "Users/register"
         GlobalScope.launch {
             try {
                 val json = Gson().toJson(dto)
-                apiCall(json, url) { result, e ->
+                postApiCall(json, url) { result, e ->
                     if (e != null) {
                         if(e.message?.contains("IX_Users_Email") == true) {
                             callback(null, Exception("Email already exists"))
@@ -65,11 +105,128 @@ class ApiCall {
         GlobalScope.launch {
             try {
                 val json = Gson().toJson(dto)
-                apiCall(json, url) { result, e ->
+                postApiCall(json, url) { result, e ->
                     if (e != null) {
                         callback(null, Exception("Invalid username or password"))
                     }
                     else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun modifyUserAsync(dto: UserToBeStoredDTO, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/modify"
+        GlobalScope.launch {
+            try {
+                val json = Gson().toJson(dto)
+                postApiCall(json, url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error modifying user"))
+                    }
+                    else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun connectAsync(dto: ManyToManyDTO, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/connect"
+        GlobalScope.launch {
+            try {
+                val json = Gson().toJson(dto)
+                postApiCall(json, url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error connecting users"))
+                    }
+                    else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun rejectAsync(dto: ManyToManyDTO, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/reject"
+        GlobalScope.launch {
+            try {
+                val json = Gson().toJson(dto)
+                postApiCall(json, url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error rejecting users"))
+                    }
+                    else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun getFeedAsync(name: String, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/feed/$name"
+        GlobalScope.launch {
+            try{
+                getApiCall(url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error getting feed"))
+                    }
+                    else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun deleteRejectionAsync(dto: ManyToManyDTO, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/delete-rejection"
+        GlobalScope.launch {
+            try {
+                val json = Gson().toJson(dto)
+                deleteApiCall(json, url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error deleting rejection"))
+                    } else {
+                        callback(result, null)
+                    }
+                }
+            } catch (e: Exception) {
+                callback(null, e)
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun deleteConnectionAsync(dto: ManyToManyDTO, callback: (String?, Exception?) -> Unit) {
+        val url = mainUrl + "Users/delete-connection"
+        GlobalScope.launch {
+            try {
+                val json = Gson().toJson(dto)
+                deleteApiCall(json, url) { result, e ->
+                    if (e != null) {
+                        callback(null, Exception("Error deleting connection"))
+                    } else {
                         callback(result, null)
                     }
                 }
