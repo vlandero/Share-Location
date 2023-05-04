c685c8f7b4e903c714243f2a2099392b72b4158c
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-05-05 02:43:26 +0300

    made bottom nav drawer

--- Mobile/app/src/main/java/com/example/mobile/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/mobile/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.mobile
 import ApiCall
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.mobile.DTOs.UserRegisterRequestDTO
 import com.example.mobile.DTOs.UserToBeStoredDTO
 import com.example.mobile.api.Auth
+import com.example.mobile.databinding.ActivityMainBinding
+import com.example.mobile.fragments.Home
+import com.example.mobile.fragments.Login
+import com.example.mobile.fragments.Register
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,9 +18,28 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home_button -> replaceFragment(Home())
+                R.id.login_button -> replaceFragment(Login())
+                R.id.register_button -> replaceFragment(Register())
+
+                else ->{
+
+
+                }
+            }
+            true
+        }
+
         val apiButton = findViewById<android.widget.Button>(R.id.buttonApi);
         val apiCall = ApiCall()
         val gson = Gson()
@@ -35,5 +59,12 @@ class MainActivity : AppCompatActivity() {
                 println("after")
             }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
