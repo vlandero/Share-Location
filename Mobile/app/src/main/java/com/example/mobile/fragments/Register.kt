7c18dbf2396c7b8fc0d46baaf630e7727337252d
iulitaalpetri <iulitaalpetri66@gmail.com>
2023-06-11 23:01:25 +0300

    test

--- Mobile/app/src/main/java/com/example/mobile/fragments/Register.kt
+++ Mobile/app/src/main/java/com/example/mobile/fragments/Register.kt
@@ -25,6 +25,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Register.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+///testttt
 class Register : Fragment()  {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
