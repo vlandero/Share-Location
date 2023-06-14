1ccb9da74de45f2f909900a098cf7a58100a828b
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-06-14 21:24:52 +0300

    remove home fragment because is unnecessary

--- Mobile/app/src/main/java/com/example/mobile/fragments/Explore.kt
+++ Mobile/app/src/main/java/com/example/mobile/fragments/Explore.kt
@@ -15,17 +15,8 @@ import com.example.mobile.adapters.UserCardAdapter
 import com.yuyakaido.android.cardstackview.*
 import java.io.Serializable
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_USER = "ARG_USER"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Explore.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Explore : Fragment(), CardStackListener {
-    // TODO: Rename and change types of parameters
     private var paramUser: UserToBeStoredDTO? = null
     private var userArray = ArrayList<UserToBeStoredDTO>()
     private val apiCall = ApiCall()
@@ -79,25 +70,6 @@ class Explore : Fragment(), CardStackListener {
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Explore.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(user: UserToBeStoredDTO) =
-            Explore().apply {
-                arguments = Bundle().apply {
-                    putSerializable(ARG_USER, user as Serializable)
-                }
-            }
-    }
-
     override fun onCardDragging(direction: Direction?, ratio: Float) {
     }
 
@@ -136,5 +108,14 @@ class Explore : Fragment(), CardStackListener {
     }
 
     override fun onCardDisappeared(view: View?, position: Int) {
+    }
+    companion object {
+        @JvmStatic
+        fun newInstance(user: UserToBeStoredDTO) =
+            Explore().apply {
+                arguments = Bundle().apply {
+                    putSerializable(ARG_USER, user as Serializable)
+                }
+            }
     }
 }
