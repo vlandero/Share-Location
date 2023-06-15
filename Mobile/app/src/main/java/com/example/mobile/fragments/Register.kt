5fea73319bdb01b6ceac116a5d085e669901367e
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-06-15 03:06:20 +0300

    working

--- Mobile/app/src/main/java/com/example/mobile/fragments/Register.kt
+++ Mobile/app/src/main/java/com/example/mobile/fragments/Register.kt
@@ -6,10 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.widget.*
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 import com.example.mobile.DTOs.UserLoginRequestDTO
 import com.example.mobile.DTOs.UserRegisterRequestDTO
 import com.example.mobile.DTOs.UserToBeStoredDTO
